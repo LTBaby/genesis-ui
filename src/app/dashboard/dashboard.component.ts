88038ea7c5d024765b63e4f93baef325f48c11f3
LT69Baby <trevor998tredoux@gmail.com>
2024-03-13 22:58:03 +0200

    added navigation

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  constructor(public router: Router, private route: ActivatedRoute) {}
+  ngOnInit(){
+    let currentRoute = this.route.root;
+    console.log(this.router.url)
 
+  }
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,6 +6,8 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
+import { Router, ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -14,5 +16,10 @@ import {MatListModule} from '@angular/material/list';
   imports: [MatSidenavModule, MatFormFieldModule, MatSelectModule, MatButtonModule, MatIconModule, MatDividerModule, MatListModule],
 })
 export class SidebarComponent {
+  constructor(public router: Router, private route: ActivatedRoute) {}
+  ngOnInit(){
+    let currentRoute = this.route.root;
+    console.log(currentRoute.snapshot.url)
 
+  }
 }
